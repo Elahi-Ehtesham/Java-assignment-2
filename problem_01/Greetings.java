03406a298601c2b1c19376b2f45593f11adc9e27
Elahi <marufeelahi@gmail.com>
2023-07-19 09:34:59 +0600

    initial commit

---
+++ problem_01/Greetings.java
@@ -0,0 +1,16 @@
+package problem_01;
+
+public class Greetings {
+    public void sayHi(){
+        System.out.println("Hi!");
+    }
+    public void sayNamaste(){
+        System.out.println("Namaste");
+    }
+    public void sayBonjour(){
+        System.out.println("Bonjour!");
+    }
+    public void sayShagotom(){
+        System.out.println("Shagotom!");
+    }
+}
---
+++ problem_06/Friends.java
@@ -0,0 +1,13 @@
+package problem_06;
+
+public class Friends {
+    public static void main(String[] args) {
+        String item1 = "Rafid";
+        String item2 = "Himel";
+        String item3 = "Ashfak";
+
+        System.out.println(item1);
+        System.out.println(item2);
+        System.out.println(item3);
+    }
+}
